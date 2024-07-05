64e911ab96337f08a8b6cb9bfafa774731bc88ea
Júlia Lima <juliafclima@hotmail.com>
2024-07-05 14:40:54 -0300

    fireEvent

---
+++ src/components/SelectList/selectList.test.tsx
@@ -0,0 +1,17 @@
+import { fireEvent, render, screen } from "@testing-library/react-native";
+
+import { SelectList } from "@components/SelectList";
+
+describe("Component: SelectList", () => {
+  it("should be return city details selected | deve ser retornado os detalhes da cidade selecionados", async () => {
+    const data = [
+      { id: "1", name: "Campinas", latitude: 123, longitude: 456 },
+      { id: "2", name: "Campo grande", latitude: 123, longitude: 456 },
+    ];
+
+    render(<SelectList data={data} onChange={() => {}} onPress={() => {}} />);
+
+    const selectedCity = screen.getByText(/campo/i);
+        fireEvent.press(selectedCity);
+  });
+});
