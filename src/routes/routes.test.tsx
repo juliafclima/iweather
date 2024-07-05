c93106d6912eb77c215038f1711d4a0fe85e96a5
Júlia Lima <juliafclima@hotmail.com>
2024-07-05 16:49:27 -0300

    CustomRender

---
+++ src/routes/routes.test.tsx
@@ -0,0 +1,29 @@
+import { screen, waitFor } from "@testing-library/react-native";
+
+import { render } from "@__tests__/utils/customRender";
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
+import { Routes } from "@routes/index";
+
+describe("Routes", () => {
+  it("should be render Search screen when not city selecte", async () => {
+    render(<Routes />);
+
+    const title = await waitFor(() => screen.findByText(/^escolha um local/i));
+
+    expect(title).toBeTruthy();
+  });
+
+  it("shoul be render Dashboard screen when has city selected", async () => {
+    const city = {
+      id: "1",
+      name: "São Paulo",
+      latitude: 123,
+      longitude: 456,
+    };
+
+    await saveStorageCity(city);
+
+    const { debug } = render(<Routes />);
+    debug();
+  });
+});
